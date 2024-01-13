c69398d995f9995cc011d70f69b959648f64efdf
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-01-13 12:50:10 -0800

    feat(page): landing
    Design is good

---
+++ src/constant/employees.ts
@@ -0,0 +1,32 @@
+export type employeeType = {
+  name: string;
+  jobTitle: string;
+  image: string;
+};
+
+export const employees: employeeType[] = [
+  {
+    name: 'Leonard Kraskner',
+    jobTitle: 'Real Estate Agent',
+    image:
+      'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+  },
+  {
+    name: 'Leonard Kraskner',
+    jobTitle: 'Real Estate Agent',
+    image:
+      'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+  },
+  {
+    name: 'Leonard Kraskner',
+    jobTitle: 'Real Estate Agent',
+    image:
+      'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+  },
+  {
+    name: 'Leonard Kraskner',
+    jobTitle: 'Real Estate Agent',
+    image:
+      'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+  },
+];
